a40d86355fdfdca6011f7bc84d83fe4bf945392f
adamgothmann <adamgothmann@gmail.com>
2017-02-18 16:05:42 -0600

    create animal is working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,8 @@
-console.log('in script');
-
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
-  console.log('in angular');
+myApp.controller('mainController', ['$scope', '$http', '$window', function($scope, $http, $window){
   var key;
+  $scope.animals = [];
   $scope.key = function(){
     $http({
       method: 'GET',
@@ -21,7 +19,7 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
       url: 'https://animalrestapi.azurewebsites.net/Animal/List?candidateID=' + key + '',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
-      }
+      },
     }).then(function(response){
       $scope.animals = response.data.list;
       console.log($scope.animals);
@@ -32,9 +30,26 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
       commonName: $scope.commonName,
       scientificName: $scope.scientificName,
       family: $scope.family,
-      url: $scope.url
+      imageURL: $scope.imageURL
     };
     console.log(animalToSend);
+    //   url: 'https://animalrestapi.azurewebsites.net/Animal/Create',
+    //   headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    //   data: $.param({
+    //     commonName: $scope.commonName,
+    //     scientificName: $scope.scientificName,
+    //     family: $scope.family,
+    //     imageURL: $scope.imageURL,
+    //     candidateID: key
+    //   })
+    $http({
+      method: 'POST',
+      url: 'https://animalrestapi.azurewebsites.net/Animal/Create?commonName=' + $scope.commonName + '&scientificName=' + $scope.scientificName + '&family=' + $scope.family + '&imageURL=' + $scope.imageURL + '&candidateID=' + key + '',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+    // $window.location.reload();
   };
 $scope.key();
 }]);
